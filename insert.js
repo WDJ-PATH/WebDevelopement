e59e6e5b3e8c5ad8ca12c79b84bd973e1a0b0fdb
aswinrprasad <aswinrprasad@gmail.com>
2018-11-16 09:37:52 +0530

    Inserting Json data to MongoDB

---
+++ insert.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('nvdcve-1.0-2002.json', 'utf8'));
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("nvd");
+  
+  dbo.collection("general").insertOne(obj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
